430bdee732c318d44490a86526197e19741e8336
Ezriel Ciriaco <ej.ciriaco@gmail.com>
2024-05-04 13:30:32 -0400

    Update password

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "myapp-mysqldb-use1.cysjtc0l6kjf.us-east-1.rds.amazonaws.com",
   USER: "username",
-  PASSWORD: "*lwJS?c-+y7xs7#z[5ah)Pj<z!1D",
+  PASSWORD: "?*U}{rVjW7LlL*-e3XbY8hAYke4*",
   DB: "myappmysql",
   dialect: "mysql",
   pool: {
