c730576114de3a3b1c1c0f0d7df746e5a3b4e2d6
Ezriel Ciriaco <ej.ciriaco@gmail.com>
2024-05-04 10:03:20 -0400

    Init

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "myapp-mysqldb-use1.cysjtc0l6kjf.us-east-1.rds.amazonaws.com",
+  USER: "username",
+  PASSWORD: "*lwJS?c-+y7xs7#z[5ah)Pj<z!1D",
+  DB: "myappmysql",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
