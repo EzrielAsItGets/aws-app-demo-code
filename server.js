415a57e4249360880cc1fe712d3cf81d2f4ca5e5
Ezriel Ciriaco <ej.ciriaco@gmail.com>
2024-05-05 18:19:01 -0400

    Enable DB again

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//const db = require("./app/models");
-//
-//db.sequelize.sync();
+const db = require("./app/models");
+
+db.sequelize.sync();
 // // drop the table if it already exists
 // db.sequelize.sync({ force: true }).then(() => {
 //   console.log("Drop and re-sync db.");
