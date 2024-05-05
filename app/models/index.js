924b199757f10e54d06e28876afe555de250297c
Ezriel Ciriaco <ej.ciriaco@gmail.com>
2024-05-05 11:18:07 -0400

    Test

--- app/models/index.js
+++ app/models/index.js
@@ -21,4 +21,15 @@ db.sequelize = sequelize;
 
 db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
 
+// Test the database connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+    process.exit(1); // Exit with a non-zero status code to indicate failure
+  });
+
 module.exports = db;
