405e936ee655eae9af05727b1efcb34efc67e48d
Ezriel Ciriaco <ej.ciriaco@gmail.com>
2024-05-05 01:40:10 -0400

    revert

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: process.env.HOST,
   USER: process.env.USER,
-  PASSWORD: "#JE}ZNY27+u4da*nrazhmP3?k5Pm",//process.env.PASSWORD,
+  PASSWORD: process.env.PASSWORD,
   DB: process.env.DBNAME,
   dialect: "mysql",
   pool: {
