2740d3a490db6ef18c47e62cfc753baafdbb9180
Ezriel Ciriaco <ej.ciriaco@gmail.com>
2024-05-05 20:21:55 -0400

    Revert

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var corsOptions = {
   origin: "http://localhost:8081"
 };
 
-//app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+//app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
